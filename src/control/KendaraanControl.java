dc363fb0d36b411fde89e3f7cc070fcf603f7027
Fredo Code <128687596+Fredo-Ronan@users.noreply.github.com>
2023-05-08 10:39:05 +0700

    Big Modification Update on KendaraaanControl.java
    Ini aku udah ketemu tris buat yg simpen GenerateID tpi ambilnya dari ID yg tersimpan di database nya, coba cek tris

--- src/control/KendaraanControl.java
+++ src/control/KendaraanControl.java
@@ -66,5 +66,153 @@ public class KendaraanControl {
     }
     
  
+    public int getLastIdMobil(){
+        List<Kendaraan> dataKendaraan = kDao.showData();
+        int lastIdMobil = -1;
+        String lastIdMobilStr = "";
+        String first, last;
+        
+        for(int i = 0; i<dataKendaraan.size(); i++){
+            if(dataKendaraan.get(i).getJenis().equalsIgnoreCase("Car")){
+                first = dataKendaraan.get(i).getId();
+                for(int j = 0; j<dataKendaraan.size(); j++){
+                    if(dataKendaraan.get(j).getJenis().equalsIgnoreCase("Car")){
+                        last = dataKendaraan.get(j).getId();
+                        if(!compareString(first, last, "Car").equalsIgnoreCase("")){
+                            lastIdMobilStr = compareString(first, last, "Car");
+                            first = last;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+
+        // MBL-1
+        // 01234 <-- index
+
+        if(!lastIdMobilStr.equals("")){
+            ArrayList<Character> strId = new ArrayList<>();
+
+            getId(strId, lastIdMobilStr);
+            
+            StringBuffer sb = new StringBuffer();
+            
+            for(Character c : strId){
+                sb.append(c);
+            }
+
+            String finalIdMobil = sb.toString();
+            lastIdMobil = Integer.parseInt(finalIdMobil);
+        }
+
+        return lastIdMobil;
+    }
     
+    public int getLastIdMotor(){
+        List<Kendaraan> dataKendaraan = kDao.showData();
+        int lastIdMotor = -1;
+        String lastIdMotorStr = "";
+        String first, last;
+        
+        for(int i = 0; i<dataKendaraan.size(); i++){
+            if(dataKendaraan.get(i).getJenis().equalsIgnoreCase("Motorcycle")){
+                first = dataKendaraan.get(i).getId();
+                for(int j = 0; j<dataKendaraan.size(); j++){
+                    if(dataKendaraan.get(j).getJenis().equalsIgnoreCase("Motorcycle")){
+                        last = dataKendaraan.get(j).getId();
+                        if(!compareString(first, last, "Motorcycle").equalsIgnoreCase("")){
+                            lastIdMotorStr = dataKendaraan.get(i).getId();
+                        }
+                    }
+                }
+                break;
+            }
+        }
+
+        // MTR-1
+        // 01234 <-- index
+
+        if(!lastIdMotorStr.equals("")){
+            ArrayList<Character> strId = new ArrayList<>();
+
+            getId(strId, lastIdMotorStr);
+            
+            StringBuffer sb = new StringBuffer();
+            
+            for(Character c : strId){
+                sb.append(c);
+            }
+
+            String finalIdMotor = sb.toString();
+            lastIdMotor = Integer.parseInt(finalIdMotor);
+        }
+
+        return lastIdMotor;
+    }
+    
+    public void getId(ArrayList<Character> destination, String id){
+        for(int i = 4; i<id.length(); i++){
+            destination.add(id.charAt(i));
+        }
+    }
+    
+    public String compareString(String idFirst, String idSecond, String jenis){
+        int defaultMobil = 264;
+        int defaultMotor = 243;
+        int tempFirst = 0;
+        int tempSecond = 0;
+        
+        if(jenis.equalsIgnoreCase("Car")){
+            for(int i = 4; i<idFirst.length(); i++){
+                tempFirst = tempFirst + idFirst.charAt(i);
+            }
+            
+            tempFirst = tempFirst + defaultMobil;
+            
+            for(int i = 4; i<idSecond.length(); i++){
+                tempSecond = tempSecond + idSecond.charAt(i);
+            }
+            
+            tempSecond = tempSecond + defaultMobil;
+            
+            if(tempSecond > tempFirst){
+                return idSecond;
+            } else if(tempSecond < tempFirst){
+                return idFirst;
+            } else {
+                if(idSecond.charAt(idSecond.length() - 1) < idSecond.charAt(idSecond.length() - 2)){
+                    return idSecond;
+                } else {
+                    return idFirst;
+                }
+            }
+        } else if(jenis.equalsIgnoreCase("Motorcycle")){
+            for(int i = 4; i<idFirst.length(); i++){
+                tempFirst = tempFirst + idFirst.charAt(i);
+            }
+            
+            tempFirst = tempFirst + defaultMotor;
+            
+            for(int i = 4; i<idSecond.length(); i++){
+                tempSecond = tempSecond + idSecond.charAt(i);
+            }
+            
+            tempSecond = tempSecond + defaultMotor;
+            
+            if(tempSecond > tempFirst){
+                return idSecond;
+            } else if(tempSecond < tempFirst){
+                return idFirst;
+            } else {
+                if(idSecond.charAt(idSecond.length() - 1) < idSecond.charAt(idSecond.length() - 2)){
+                    return idSecond;
+                } else {
+                    return idFirst;
+                }
+            }
+        }
+        
+        return "";
+    }
 }
